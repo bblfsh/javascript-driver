817ce336807564d49e55c63ccd291998ed918330
Denys Smirnov <denys@sourced.tech>
2018-04-09 21:41:45 +0200

    porting annotations to a new DSL
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

---
+++ driver/fixtures/fixtures_test.go
@@ -0,0 +1,34 @@
+package fixtures
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/bblfsh/javascript-driver/driver/normalizer"
+	"gopkg.in/bblfsh/sdk.v1/sdk/driver"
+	"gopkg.in/bblfsh/sdk.v1/sdk/driver/fixtures"
+)
+
+const projectRoot = "../../"
+
+var Suite = &fixtures.Suite{
+	Lang: "javascript",
+	Ext:  ".js",
+	Path: filepath.Join(projectRoot, fixtures.Dir),
+	NewDriver: func() driver.BaseDriver {
+		return driver.NewExecDriverAt(filepath.Join(projectRoot, "build/bin/native"))
+	},
+	Transforms: driver.Transforms{
+		Native: normalizer.Native,
+		Code:   normalizer.Code,
+	},
+	BenchName: "u2_class_method", // TODO: specify a largest file
+}
+
+func TestJavascriptDriver(t *testing.T) {
+	Suite.RunTests(t)
+}
+
+func BenchmarkJavascriptDriver(b *testing.B) {
+	Suite.RunBenchmarks(b)
+}
