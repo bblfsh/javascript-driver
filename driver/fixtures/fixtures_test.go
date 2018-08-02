ec8b579be5a2236fdc90f45b6e7542dd2045262f
Denys Smirnov <denys@sourced.tech>
2018-08-02 20:27:03 +0300

    support v2 protocol
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- driver/fixtures/fixtures_test.go
+++ driver/fixtures/fixtures_test.go
@@ -5,8 +5,9 @@ import (
 	"testing"
 
 	"github.com/bblfsh/javascript-driver/driver/normalizer"
-	"gopkg.in/bblfsh/sdk.v2/sdk/driver"
-	"gopkg.in/bblfsh/sdk.v2/sdk/driver/fixtures"
+	"gopkg.in/bblfsh/sdk.v2/driver"
+	"gopkg.in/bblfsh/sdk.v2/driver/fixtures"
+	"gopkg.in/bblfsh/sdk.v2/driver/native"
 )
 
 const projectRoot = "../../"
@@ -15,8 +16,8 @@ var Suite = &fixtures.Suite{
 	Lang: "javascript",
 	Ext:  ".js",
 	Path: filepath.Join(projectRoot, fixtures.Dir),
-	NewDriver: func() driver.BaseDriver {
-		return driver.NewExecDriverAt(filepath.Join(projectRoot, "build/bin/native"))
+	NewDriver: func() driver.Native {
+		return native.NewDriverAt(filepath.Join(projectRoot, "build/bin/native"), native.UTF8)
 	},
 	Transforms: normalizer.Transforms,
 	BenchName:  "u2_class_method", // TODO: specify a largest file
