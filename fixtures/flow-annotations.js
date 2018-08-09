10ec5ae13bd2196137db4303fec0d98984809ac1
Juanjo Alvarez <juanjo@sourced.tech>
2018-08-09 09:35:45 +0200

    Tests for TypeParameterInstantiation, ObjectTypeAnnotation and ObjectTypeProperty
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- fixtures/flow-annotations.js
+++ fixtures/flow-annotations.js
@@ -3,9 +3,15 @@
 export default class PluginPass {
     testfnc1(a: string, b: ?string, c: number, d: mixed, e: Object){}
     testfnc2(f: string | boolean, g: boolean, h: null, i: void){}
-    testfncd(j: "one" | "other"){}
+    testfnc3(j: "one" | "other"){}
+    testfnc_object(node: ?{
+      loc?: { start: { line: number, column: number } },
+      _loc?: { start: { line: number, column: number } },
+    }) {}
 
     num: number = 1;
     s: string = "foo";
     pok: File;
+    _map: Map<mixed, mixed> = Map();
+    
 }
