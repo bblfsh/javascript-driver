66dc94af815eea82b068ba74bc4ac4c3745a34b1
Denys Smirnov <denys@sourced.tech>
2019-01-22 18:29:54 +0200

    update sdk and regenerate fixtures
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- driver/normalizer/transforms.go
+++ driver/normalizer/transforms.go
@@ -3,9 +3,10 @@ package normalizer
 import "gopkg.in/bblfsh/sdk.v2/driver"
 
 var Transforms = driver.Transforms{
-	Namespace:   "javascript",
-	Preprocess:  Preprocess,
-	Normalize:   Normalize,
-	Annotations: Native,
-	Code:        Code,
+	Namespace:      "javascript",
+	Preprocess:     Preprocess,
+	PreprocessCode: PreprocessCode,
+	Normalize:      Normalize,
+	Annotations:    Native,
+	Code:           Code,
 }
