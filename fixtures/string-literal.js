399f6ed4d972ec132ccf5ab94c2569c8b3baec31
Alexander Bezzubov <bzz@apache.org>
2019-03-07 07:03:43 +0100

    test: add fixtures to reproduce the failure
    Signed-off-by: Alexander Bezzubov <bzz@apache.org>

--- fixtures/string-literal.js
+++ fixtures/string-literal.js
@@ -1,3 +1,9 @@
 a = "a"
 u8 = "ё"
 bc = "b\nc"
+
+var escSlash = '\0SLASH'+Math.random()+'\0';
+var escOpen = '\0OPEN'+Math.random()+'\0';
+var escClose = '\0CLOSE'+Math.random()+'\0';
+var escComma = '\0COMMA'+Math.random()+'\0';
+var escPeriod = '\0PERIOD'+Math.random()+'\0';
