9e890dfca7907e4ddfec758416b6a5b998592fad
Alexander Bezzubov <bzz@apache.org>
2019-03-27 00:51:55 +0100

    sem: a fixture to reproduce #75
    Signed-off-by: Alexander Bezzubov <bzz@apache.org>

--- fixtures/string-literal.js
+++ fixtures/string-literal.js
@@ -7,3 +7,5 @@ var escOpen = '\0OPEN'+Math.random()+'\0';
 var escClose = '\0CLOSE'+Math.random()+'\0';
 var escComma = '\0COMMA'+Math.random()+'\0';
 var escPeriod = '\0PERIOD'+Math.random()+'\0';
+
+d = "\0\0\0\0\0\0\0\0"
